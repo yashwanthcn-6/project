df6ea38a1dd1fd5ccd26eb9eb14c34556b7f8297
yashwanthcn-6 <yashwanthcn55@gmail.com>
2026-02-26 14:51:01 +0530

    stil in proseess

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
+let row1=document.querySelector(".sub1");
+let row2=document.querySelector(".sub2");
+let row3=document.querySelector(".sub3");
 
-let p=document.querySelector("body");
-let c=document.querySelector("h1");
-let r="red";
-c.addEventListener("click",()=>{
-    c.innerHTML="Hello World";
-}
-    )
-let abc=()=>{
-    if(r== "red"){
-        p.style.backgroundColor="red";
-        c.style.color="blue";
-       r="blue";}
-    else {
-         p.style.backgroundColor="blue";
-         c.style.color="red";
-         r="red";}
-    }
-    
-a.addEventListener("click",abc)
 
+let b=document.querySelector("body");
+let h1=document.querySelector("h1");
+
+let a=document.querySelector("button")
+
+let conts=document.querySelectorAll(".cont");
+let change="x";
+
+    conts.forEach((on) => {
+        
+        on.addEventListener("click", () => {
+            let p = on.querySelector("p.res");
+            
+            // if cell already has content (x or 0), freeze it - don't change
+            if(p.textContent !== "") return;
+            
+                 if(change == "x"){
+                    p.textContent = "x";
+                      change="0";
+                 }
+                 else{
+                    p.textContent = "0";
+                    change="x";
+                 }
+                 
+                
+        });
+        })
     
