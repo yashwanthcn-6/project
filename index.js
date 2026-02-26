c88912866f1fe8e554e0e902c6c3c3c7320db83b
yashwanthcn-6 <yashwanthcn55@gmail.com>
2026-02-26 22:12:56 +0530

    adding patterns

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let row1=document.querySelector(".sub1");
 let row2=document.querySelector(".sub2");
 let row3=document.querySelector(".sub3");
+const winner = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
 
 let b=document.querySelector("body");
@@ -28,7 +38,20 @@ let change="x";
                     change="x";
                  }
                  
-                
+                checkwinner();
         });
         })
     
+const checkwinner=()=>{
+    for(let pattern of winner){
+
+        console.log(pattern[0],pattern[1],pattern[2]);
+         console.log(winner[pattern[0]],winner[pattern[1]],winner[pattern[2]]);
+          console.log(
+            conts[pattern[0]].textContent,
+            conts[pattern[1]].textContent,
+            conts[pattern[2]].textContent
+        );
+    }
+
+    }
